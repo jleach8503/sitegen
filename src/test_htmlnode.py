f2095bc8befcdefebbc67bf5b284f987067b7943
jleach8503 <jonathanm.leach@gmail.com>
2026-03-25 21:59:04 +0000

    Added LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -26,6 +26,39 @@ class TestHTMLNode(unittest.TestCase):
     def test_empty_props_to_html(self):
         node = HTMLNode()
         self.assertEqual(node.props_to_html(), "")
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_h1(self):
+        node = LeafNode("h1", "Hello, world!")
+        self.assertEqual(node.to_html(), "<h1>Hello, world!</h1>")
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "Hello, world!")
+        self.assertEqual(node.to_html(), "<b>Hello, world!</b>")
+
+    def test_empty_leaf_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_repr(self):
+        props = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        node = LeafNode("a", "somevalue", props)
+        expected = 'LeafNode(a, somevalue, href="https://www.google.com" target="_blank")'
+        self.assertEqual(str(node), expected)
+
+    def test_empty_leaf_value(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode("a", None)
+            node.to_html()
+        
 
 
 if __name__ == "__main__":
