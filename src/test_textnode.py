2ede1edf75f2eda78d734437106cced18127ab2a
jleach8503 <jonathanm.leach@gmail.com>
2026-03-25 22:46:07 +0000

    Added text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +23,52 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a link node", TextType.LINK, link)
         self.assertEqual(node.url, link)
 
+
+class TestTextNodeToHtmlNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_bold(self):
+        node = TextNode("This is an italic node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic node")
+
+    def test_code(self):
+        node = TextNode("This is a code node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code node")
+
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node")
+        self.assertEqual(html_node.props["href"], "https://www.google.com")
+
+    def test_image(self):
+        node = TextNode("This is an image node", TextType.IMAGE, "https://www.google.com/mypic.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props["src"], "https://www.google.com/mypic.png")
+        self.assertEqual(html_node.props["alt"], "This is an image node")
+
+    def test_invalid_type(self):
+        with self.assertRaises(ValueError) as e:
+            node = TextNode("This is an invalid node", "superduper")
+            text_node_to_html_node(node)
+        self.assertIn("invalid text_type: superduper", str(e.exception))
 
 if __name__ == "__main__":
     unittest.main()
