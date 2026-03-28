9d1e3074120028e81f3ffe25d218183651557c18
jleach8503 <jonathanm.leach@gmail.com>
2026-03-28 12:27:50 +0000

    Configured building into docs

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
 import os
+import sys
 import shutil
 from markdown_blocks import markdown_to_html_node, extract_title
 
 
 BASE_DIR = os.path.dirname(os.path.split(os.path.abspath(__file__))[0])
 STATIC_DIR = os.path.join(BASE_DIR, "static")
-PUBLIC_DIR = os.path.join(BASE_DIR, "public")
 CONTENT_DIR = os.path.join(BASE_DIR, "content")
+OUTPUT_DIR = os.path.join(BASE_DIR, "docs")
+
 
 def copy_asset(source, destination, clean=False):
     if clean and os.path.exists(destination):
         shutil.rmtree(destination)
-        os.mkdir(destination)
 
+    os.makedirs(destination, exist_ok=True)
     for name in os.listdir(source):
         source_path = os.path.join(source, name)
         target_path = os.path.join(destination, name)
@@ -26,7 +28,7 @@ def copy_asset(source, destination, clean=False):
         copy_asset(source_path, target_path, False)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f:
         markdown = f.read()
@@ -36,35 +38,46 @@ def generate_page(from_path, template_path, dest_path):
 
     html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    content = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    if not basepath.endswith("/"):
+        basepath += "/"
+    content = (
+        template
+            .replace("{{ Title }}", title)
+            .replace("{{ Content }}", html)
+            .replace('href="/', f'href="{basepath}')
+            .replace('src="/',f'src="{basepath}')
+    )
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(content)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for name in os.listdir(dir_path_content):
         source_path = os.path.join(dir_path_content, name)
         target_path = os.path.join(dest_dir_path, name)
 
         if os.path.isfile(source_path) and os.path.splitext(source_path)[1] == ".md":
             target_path = os.path.splitext(target_path)[0] + ".html"
-            generate_page(source_path, template_path, target_path)
+            generate_page(source_path, template_path, target_path, basepath)
             continue
 
         if os.path.isdir(source_path):            
-            generate_pages_recursive(source_path, template_path, target_path)
+            generate_pages_recursive(source_path, template_path, target_path, basepath)
             continue
     
 
 
 def main():
-    copy_asset(STATIC_DIR, PUBLIC_DIR, True)
+    basepath = sys.argv[0] if len(sys.argv[0]) > 1 else "/"    
+
+    copy_asset(STATIC_DIR, OUTPUT_DIR, True)
     generate_pages_recursive(
         CONTENT_DIR,
         os.path.join(BASE_DIR, "template.html"),
-        PUBLIC_DIR,
+        OUTPUT_DIR,
+        basepath,
     )
 
 
